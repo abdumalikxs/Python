27de22ee6d6efa43cc24d7f21c47992b4badd1b3
Abdumalik <adalerzoda3@gmail.com>
2025-02-23 20:30:21 -0500

    Binary convert with int + rounding with decimal places to round off to

--- think.py
+++ think.py
@@ -2,4 +2,4 @@ import math
 
 # int can take a second argument that specifies the base of the number.
 print(int('111', 2))
-print(round(math.pi, 4))
+print(round(math.pi, 4))  # number of decimal places to round off to.
